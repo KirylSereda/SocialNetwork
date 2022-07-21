9302e0a78047ad16f51ce3c01fe387a75a438272
dsadas <seredakiryl@gmail.com>
2022-07-21 14:25:12 +0300

    25:useEffect Return side effect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react"
-
+import React, { useEffect, useState } from "react"
 
 export default {
     title: 'Effect',
 }
-
-
 
 export const SimpleExample = () => {
     const [counter, setCounter] = useState(0)
@@ -33,22 +30,45 @@ export const SimpleExample = () => {
     </div>
 }
 
-export const SimpleTimeoutExample = () => {
+
+export const ResetEffectExample = () => {
     const [counter, setCounter] = useState(0)
-    const [fake, setFake] = useState(0)
-    console.log('SimpleTimeoutExample')
+
+    console.log('component rendered with' + counter)
 
     useEffect(() => {
-        setInterval(() => {
-            setCounter((counter) => counter + 1)
-        }, 1000)
+        console.log('Effect occurred' + counter)
+
+        return () => {
+            console.log('RESET EFFECT' + counter)
+        }
     }, [])
 
+    const increase = () => setCounter(counter + 1)
+    return <div>
+        conter:{counter}
+        <button onClick={increase}></button>
+    </div >
+}
+
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState('')
+
+    console.log('component rendered with' + text)
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+            window.addEventListener('keypress', handler)
+            return () => {
+                window.removeEventListener('keypress', handler)
+            }
+        }
+    }, [text])
 
     return <div>
-        <div>counter:{counter}   fake:{fake}</div>
-
-        {/* <button onClick={() => setFake(fake + 1)}>+</button>
-        <button onClick={() => setCounter(counter + 1)}>+</button> */}
+        Typed text:{text}
     </div >
 }
